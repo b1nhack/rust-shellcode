df79e385a8f7c80039043621ef2c4235e7b70284
test <test@test.com>
2023-03-20 07:33:21 -0700

    add asm

---
+++ asm/src/main.rs
@@ -0,0 +1,14 @@
+use std::arch::asm;
+
+#[link_section = ".text"]
+static SHELLCODE: [u8; 113] = *include_bytes!("../../win-exec-calc-shellcode.bin");
+
+#[cfg(target_os = "windows")]
+fn main() {
+    unsafe {
+        asm!(
+        "call {}",
+        in(reg) SHELLCODE.as_ptr(),
+        )
+    }
+}
